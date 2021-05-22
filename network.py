7aadffb1d0bace345570a442d6a491c7c18ace3b
rodmafra92 <yues.mafra@gmail.com>
2021-05-22 16:24:20 -0300

    first commit

---
+++ network.py
@@ -0,0 +1,31 @@
+import socket
+
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.100.35"
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.pos = self.connect()
+        print(self.pos)
+    
+    def getPos(self):
+        return self.pos
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+    
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).dsecode()
+        except socket.error as e:
+            print(e)
+
+n = Network()
+print(n.send("Working"))
